7042012c46bb78cc57acb69994b18a87c2cb1f21
danipereiracodes <danipg.dev@gmail.com>
2023-08-23 19:14:29 +0200

    added kern burns effect to homebanner

--- src/components/HomeBanner.tsx
+++ src/components/HomeBanner.tsx
@@ -1,14 +1,16 @@
 export const HomeBanner: React.FC = () => {
   return (
-    <section className=" relative flex h-screen flex-col">
+    <section className="relative flex h-screen">
       <div
         style={{
           backgroundImage: `url('/desk.jpeg')`,
         }}
-        className="flex flex-1 flex-col items-center justify-center justify-items-center justify-self-center bg-cover bg-center"
+        className="ken-burns-background absolute inset-0 bg-cover bg-center"
       >
         <div className="absolute inset-0 bg-black opacity-60"></div>
-        <div className="lg:max-w-3/4 z-10 w-5/6 text-center">
+      </div>
+      <div className="relative z-10 mx-auto flex flex-col items-center justify-center justify-items-center justify-self-center">
+        <div className="lg:max-w-3/4 w-5/6 text-center">
           <h1 className="py-12 text-4xl font-extrabold text-slate-200 lg:text-6xl">
             # Front-end Software Engineer
           </h1>
