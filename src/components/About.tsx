9f636196c6deb50f5c04e73773cd17949a4f2371
danipereiracodes <danipg.dev@gmail.com>
2023-09-19 20:04:19 +0200

    brush stroke to about image

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -26,21 +26,21 @@ export const About = () => {
       <div className=" flex flex-col-reverse items-center md:flex-row-reverse md:gap-16">
         <div className=" flex flex-col gap-4 pt-16 grayscale  md:w-[50%] md:items-center">
           <Image
-            className="about-image z-20  rotate-12  transform transform transform rounded-full transition-transform duration-300 ease-in-out hover:rotate-6"
+            className="about-image pointer-events-none z-20  rotate-12  transform transform transform rounded-full transition-transform duration-300 ease-in-out hover:rotate-6"
             src="/cropped/row-1-column-1.jpg"
             width={500}
             height={300}
             alt="about me picture"
           />
           <Image
-            className="about-image z-10  rounded-full opacity-80 transition-transform duration-300 ease-in-out hover:rotate-[-15deg]"
+            className="about-image pointer-events-none z-10  rounded-full opacity-80 transition-transform duration-300 ease-in-out hover:rotate-[-15deg]"
             src="/cropped/row-2-column-1.jpg"
             width={500}
             height={300}
             alt="about me picture"
           />
           <Image
-            className="about-image o  z-30 rotate-[12deg] rounded-full transition-transform duration-300 ease-in-out hover:rotate-[-15deg]"
+            className="about-image o pointer-events-none  z-30 rotate-[12deg] rounded-full transition-transform duration-300 ease-in-out hover:rotate-[-15deg]"
             src="/cropped/row-3-column-1.jpg"
             width={500}
             height={300}
