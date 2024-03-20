77c413a5c04f91ceacca39da266b0a102ff129ba
danipereiracodes <danipg.dev@gmail.com>
2024-03-20 13:30:22 +0100

    add header padding top and bottom

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -78,7 +78,7 @@ const Header: React.FC<HeaderProps> = ({ onToggleThemeMode, ischecked }) => {
   }, [showDropdown]);
 
   return (
-    <header className='absolute top-0 z-20 flex w-screen items-center justify-center bg-[#00000059]  px-12 lg:px-0'>
+    <header className='absolute top-0 z-20 flex w-screen items-center justify-center bg-[#00000059] px-12  py-4 lg:px-0'>
       <div className='container flex w-5/6 items-center justify-center py-7 lg:py-5'>
         <Link href='/'>
           <div className='flex flex-col items-center gap-2'>
