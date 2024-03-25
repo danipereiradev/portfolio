637423cfc95688fc340e0117e7a5d17b67de3a56
danipereiracodes <danipg.dev@gmail.com>
2024-03-25 20:12:10 +0100

    move homebanner data to database

--- src/components/HomeBanner.tsx
+++ src/components/HomeBanner.tsx
@@ -1,9 +1,45 @@
 import { useTranslation } from 'react-i18next';
 import Header from './Header';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+const headers = new Headers();
+headers.append('Content-Type', 'application/json');
+headers.append(
+  'Authorization',
+  `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_KEY}`
+);
+headers.append('apikey', `${process.env.NEXT_PUBLIC_SUPABASE_KEY}`);
+
+const fetchOptions = {
+  method: 'GET',
+  headers: new Headers(headers),
+};
+
+console.log(process.env.NEXT_PUBLIC_SUPABASE_KEY);
 
 export const HomeBanner: React.FC = () => {
   const [ischecked, setIsChecked] = useState(false);
+  const [data, setData] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch(
+      'https://wajrqdbpukfrgzsdqzmg.supabase.co/rest/v1/HERO_SECTION?select=*',
+      fetchOptions
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        console.log('Data fetched successfully:', data);
+      })
+      .catch((error) => {
+        console.error('There was a problem with your fetch operation:', error);
+      });
+  }, []);
 
   const HandletoggleThemeMode = (e: React.ChangeEvent<HTMLInputElement>) => {
     setIsChecked(e.target.checked);
@@ -24,14 +60,19 @@ export const HomeBanner: React.FC = () => {
       </div>
       <Header ischecked={ischecked} onToggleThemeMode={HandletoggleThemeMode} />
       <div className='relative z-10 mx-auto flex flex-col items-center justify-items-center justify-self-center'>
-        <div className='lg:max-w-3/4 w-5/6 text-center'>
-          <h1 className='animated-title py-12 text-4xl font-extrabold text-slate-200 lg:text-6xl'>
-            {t('homeBanner.title')}
-          </h1>
-          <h2 className='animated-title text-2xl font-bold text-slate-100 md:text-3xl'>
-            {t('homeBanner.description')}
-          </h2>
-        </div>
+        {data &&
+          data.map((element) => {
+            return (
+              <div key={element.id} className='lg:max-w-3/4 w-5/6 text-center'>
+                <h1 className='animated-title py-12 text-4xl font-extrabold text-slate-200 lg:text-6xl'>
+                  {element.heading}
+                </h1>
+                <h2 className='animated-title text-2xl font-bold text-slate-100 md:text-3xl'>
+                  {element.paragraph}
+                </h2>
+              </div>
+            );
+          })}
       </div>
     </section>
   );
